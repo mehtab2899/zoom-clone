089b50aff11db56058900262692605fc42b31b43
Mehtab's PC <mehtabmultani51@gmail.com>
2021-05-11 00:18:51 +0530

    feature: project completion

--- public/script.js
+++ public/script.js
@@ -9,11 +9,11 @@ var peer = new Peer(undefined, {
 	port: "3000",
 });
 
-let myVideoStream;
+var myVideoStream;
 navigator.mediaDevices
 	.getUserMedia({
 		video: true,
-		audio: false,
+		audio: true,
 	})
 	.then((stream) => {
 		myVideoStream = stream;
@@ -51,4 +51,78 @@ const addVideoStream = (video, stream) => {
 		video.play();
 	});
 	videoGrid.append(video);
+};
+
+// sending message to chat window
+let text = document.querySelector("input");
+const html = document.querySelector("html");
+html.addEventListener("keydown", (e) => {
+	if (e.code == "Enter" && text.value.length !== 0) {
+		socket.emit("message", text.value);
+		text.value = "";
+	}
+});
+
+socket.on("createMessage", (message) => {
+	let li = document.createElement("li");
+	li.innerHTML = `<b>User</b> <br/> ${message}`;
+	document.querySelector("ul").appendChild(li);
+});
+
+// ṃute our video
+const muteUnmute = () => {
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		setUnmuteButton();
+	} else {
+		setMuteButton();
+		myVideoStream.getAudioTracks()[0].enabled = true;
+	}
+};
+
+// mute and unmute functions
+const setUnmuteButton = () => {
+	const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+    `;
+	document.querySelector(".main__mute--button").innerHTML = html;
+};
+
+const setMuteButton = () => {
+	const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span class="unmute">Unmute</span>
+    `;
+	document.querySelector(".main__mute--button").innerHTML = html;
+};
+
+// stop our video
+const playStop = () => {
+	let enabled = myVideoStream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false;
+		setPlayVideo();
+	} else {
+		setStopVideo();
+		myVideoStream.getVideoTracks()[0].enabled = true;
+	}
+};
+
+// play and stop functions
+const setStopVideo = () => {
+	const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+    `;
+	document.querySelector(".main__video--button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+	const html = `
+    <i class="stop fas fa-video-slash"></i>
+    <span class="stop">Play Video</span>
+    `;
+	document.querySelector(".main__video--button").innerHTML = html;
 };
--- server.js
+++ server.js
@@ -28,6 +28,9 @@ io.on("connection", (socket) => {
 	socket.on("join-room", (roomId, userId) => {
 		socket.join(roomId);
 		io.to(roomId).emit("user-connected", userId);
+		socket.on("message", (message) => {
+			io.to(roomId).emit("createMessage", message);
+		});
 	});
 });
 
