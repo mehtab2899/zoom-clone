89a368827dd5d49a4fa3455c0e260137772be2f1
Mehtab's PC <mehtabmultani51@gmail.com>
2021-05-10 12:13:36 +0530

    feature:adding video stream and room

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+	debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+// setting public folder
+app.use(express.static("public"));
+
+// setting ejs engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+	res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+	socket.on("join-room", (roomId, userId) => {
+		socket.join(roomId);
+		io.to(roomId).emit("user-connected", userId);
+	});
+});
+
+// listening to port
+server.listen(3000, () => {
+	console.log("server is listening to port 3000");
+});
